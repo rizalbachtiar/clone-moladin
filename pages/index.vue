2e2e836fddd4f28a5f5abfdbbe9361debb68a974
rizalbachtiar <rizal.bachtiar.31@gmail.com>
2022-01-11 00:38:14 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container mx-auto p-5">
+    <a href="#" class="px-4 py-2 bg-cyan-500 rounded-lg text-gray-100 font-light shadow-lg shadow-cyan-500/50">Create</a>
+  </div>
+</template>
